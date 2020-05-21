1ad196d1d5fd8e1d45d204f9891de704ed84a00d
seba7373 <65694797+seba7373@users.noreply.github.com>
2020-05-21 01:44:41 -0300

    Update MSSHacks.sh

--- MSSHacks.sh
+++ MSSHacks.sh
@@ -1,1 +1,2 @@
-#!/bin/bash apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/seba7373/Master/master/script.sh; chmod 777 script.sh* && ./script.sh*
+#!/bin/bash 
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/seba7373/Master/master/script.sh; chmod 777 script.sh* && ./script.sh*

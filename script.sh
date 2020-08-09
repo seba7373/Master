dca04f3434ad2f81ab9e1dac6194d864f212b1af
seba7373 <65694797+seba7373@users.noreply.github.com>
2020-08-09 19:41:57 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -176,7 +176,7 @@ msg -ama "[ NEW - ULTIMATE - SCRIPT ] ➣ \033[1;33m[\033[1;34m OFICIAL BY-DANKE
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
-Key=""
+Key="qra-atsilK0@84%ab97cda8f?K8888:8@@+95+84?+94@"
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 IP="104.238.135.147" && echo "$IP" > /usr/bin/vendor_code
 cd $HOME
